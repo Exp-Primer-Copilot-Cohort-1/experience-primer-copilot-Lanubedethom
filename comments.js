ab6f46d8dac6dc0f7f89caf453c6043a02ae3c84
Ciro <91792489+Lanubedethom@users.noreply.github.com>
2023-11-14 15:35:29 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,12 @@
-// create a web server 
-// 1. import express
-const express = require("express");
-const { request } = require("http");
-const { response } = require("express");
-// 2. create an express app
-const app = express();
+// Create web server
+var http = require('http');
+var fs = require('fs');
 
-// 3. create an array of comments
-const comments = [
-    {username: "Todd", comment: "lol that is so funny!"},
-    {username: "Skyler", comment: "I like to go birdwatching with my dog"},
-    {username: "Sk8erBoi", comment: "Plz delete your account, Todd"},
-    {username: "onlysayswoof", comment: "woof woof woof"},
-    {username: "iliketurtles", comment: "I like turtles!"}
-]
-
-// 4. create a get route for "/comments" that returns the comments array
-app.get("/comments", (request, response) => {
-    response.send(comments);
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res);
 });
 
-// 5. create a get route for "/comments/:username" where username is a path parameter that returns only the comments from the specific user in the path parameter
-app.get("/comments/:username", (request, response) => {
-    const username = request.params.username;
-    const filteredComments = comments.filter(comment => comment.username === username);
-    response.send(filteredComments);
-});
 
-// 6. start the server
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
-});
+
